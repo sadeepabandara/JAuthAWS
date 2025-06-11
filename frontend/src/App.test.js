d065c7599156ba7691f0e365421496120132859a
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-06-12 05:41:13 +1000

    update setupTest

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders user application header', () => {
     render(<App />);
-    const linkElement = screen.getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    const header = screen.getByText(/User Application/i);
+    expect(header).toBeInTheDocument();
 });
