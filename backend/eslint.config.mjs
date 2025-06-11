634514881e27c9b6c748e19f7931621d6ce6c6dd
Sadeepa Bandara <84839888+sadeepabandara@users.noreply.github.com>
2025-06-12 05:21:47 +1000

    update eslint.config

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,11 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'script',
+            globals: globals.node,
+        },
+        plugins: { js },
+        extends: ['eslint:recommended'],
+    },
 ]);
